cdbcb2398e90a906cec0dd0846ccb7da498222f8
iexeeb <84460440+iexeeb@users.noreply.github.com>
2023-02-09 01:17:55 +0900

    Add indentation

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const addDiv = (text) =>
   document.body.insertAdjacentHTML("beforeEnd", `<div>${text}</div>`);
 
-const text = JSON.stringify(UAParser(), null, 4)
-  .replaceAll("\n", "<br>")
-  .replaceAll("\u0020", "&nbsp;");
+const text = JSON.stringify(
+  UAParser(),
+  null,
+  "&nbsp;&nbsp;&nbsp;&nbsp;"
+).replaceAll("\n", "<br>");
 addDiv(text);
