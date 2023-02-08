eb2ee7a0d90e8b04f39d9f05b4076d131125a4a4
iexeeb <84460440+iexeeb@users.noreply.github.com>
2023-02-09 01:23:39 +0900

    Fix indent

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const addDiv = (text) =>
   document.body.insertAdjacentHTML("beforeEnd", `<div>${text}</div>`);
 
-const text = JSON.stringify(
-  UAParser(),
-  null,
-  "&nbsp;&nbsp;&nbsp;&nbsp;"
-).replaceAll("\n", "<br>");
+const text = JSON.stringify(UAParser(), null, "\t")
+  .replaceAll("\n", "<br>")
+  .replaceAll("\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
 addDiv(text);
